d4403189f79d92689271af60ac7106b3be22ec4c
yarllen2017-star <yarllen2017@gmail.com>
2026-04-14 20:27:57 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("app-cache").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
